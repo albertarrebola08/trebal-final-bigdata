fd604b439770c500c19f8273d00da4c78c4a1564
Albert Arrebola Sans <104431726+albertarrebola08@users.noreply.github.com>
2024-04-09 16:31:35 +0200

    Add files via upload

--- scrapping_idescat_ipc_espanya.py
+++ scrapping_idescat_ipc_espanya.py
@@ -4,19 +4,21 @@ import pandas as pd
 
 
 #url IPC
-#url = 'https://www.idescat.cat/indicadors/?id=conj&n=10261&tema=PREUS&col=1'
+url = 'https://www.idescat.cat/indicadors/?id=conj&n=10261&tema=PREUS&col=1'
 
 #url Salaris
-url = 'https://www.idescat.cat/indicadors/?id=anuals&n=10400&tema=TREBA&col=3'
+#url = 'https://www.idescat.cat/indicadors/?id=anuals&n=10400&tema=TREBA&col=3'
+
+
 
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-div_espanya = soup.find('div', id='Espanya')
-table = div_espanya.find('table')
+
+table = soup.find('table')
 df = pd.read_html(str(table))[0]
 
-df.to_csv('output.csv', index=False)
+df.to_csv('catalunya_ipc.csv', index=False)
 
 
 
